1c5ea57ee9d379b23eff31efee1dc54b5466c11e
Carl Davenport <hirecarldavenport@gmail.com>
2026-08-28 23:32:15 -0400

    Create blend tables on startup

--- podpal/db/session.py
+++ podpal/db/session.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from podpal.db.models import Base
+
 ENGINE = create_engine("sqlite:///podblendz.db", echo=False)
+
+Base.metadata.create_all(bind=ENGINE)
+
 SessionLocal = sessionmaker(bind=ENGINE)
 
 def get_session():
